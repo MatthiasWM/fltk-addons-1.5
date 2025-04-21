11b71e683888b3886771a448efc355e097491d73
Matthias Melcher <github@matthiasm.com>
2025-04-21 14:55:03 +0200

    Add simple example of adding a test app to the FLTK core.

---
+++ Fl_Range_Slider/CMake/fl_addon_test.cmake
@@ -0,0 +1,9 @@
+#
+# This is called by the core add-on feature from the FLTK core library.
+#
+# FL_ADDON_BASE is the absolute path to this add-on
+# FL_ADDON_INCLUDE is the absolute file name of this script
+#
+
+fl_create_example(range_slider ${FL_ADDON_BASE}/test/range_slider.cxx fltk::fltk)
+
